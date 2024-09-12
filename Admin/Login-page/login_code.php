fdb694b34068ada991c61208892d00435b91d962
Izumi <162671709+Yoro-Izumi@users.noreply.github.com>
2024-09-12 18:18:45 +0000

    login code for admin,super admin and user

--- Admin/Login-page/login_code.php
+++ Admin/Login-page/login_code.php
@@ -32,31 +32,31 @@ $error_message = ""; // Initialize error message variable
 
 if (isset($_POST['login'])) {
     // Sanitize input
-    $username = sanitize_input($_POST['username'], $connect_db);
+    $username = sanitize_input($_POST['email'], $connect_db);
     $password = sanitize_input($_POST['password'], $connect_db);
 
     // Check for super admin accounts
-    $stmtSuperAdmin = mysqli_prepare($connect_db, "SELECT * FROM super_admin_accounts");
+    $stmtSuperAdmin = mysqli_prepare($connect_db, "SELECT * FROM smilesync_super_admin_accounts");
     mysqli_stmt_execute($stmtSuperAdmin);
     $resultSuperAdmin = mysqli_stmt_get_result($stmtSuperAdmin);
 
     while ($superAdminAccount = mysqli_fetch_assoc($resultSuperAdmin)) {
-        $decryptedEmail = decryptData($superAdminAccount['superAdminEmail'], $key);
+        $decryptedEmail = decryptData($superAdminAccount['super_admin_email'], $key);
         if ($decryptedEmail === $username) {
-            handle_login_attempts($connect_db, $superAdminAccount['superAdminID'], 'superAdmin', $password, $superAdminAccount['superAdminPassword']);
+            handle_login_attempts($connect_db, $superAdminAccount['super_admin_account_id'], 'superAdmin', $password, $superAdminAccount['super_admin_password']);
             break;
         }
     }
 
     // Check for admin accounts
-    $stmtAdmin = mysqli_prepare($connect_db, "SELECT * FROM admin_accounts");
+    $stmtAdmin = mysqli_prepare($connect_db, "SELECT * FROM smilesync_admin_accounts");
     mysqli_stmt_execute($stmtAdmin);
     $resultAdmin = mysqli_stmt_get_result($stmtAdmin);
 
     while ($adminAccount = mysqli_fetch_assoc($resultAdmin)) {
-        $decryptedEmail = decryptData($adminAccount['adminEmail'], $key);
+        $decryptedEmail = decryptData($adminAccount['admin_email'], $key);
         if ($decryptedEmail === $username) {
-            handle_login_attempts($connect_db, $adminAccount['adminID'], 'admin', $password, $adminAccount['adminPassword']);
+            handle_login_attempts($connect_db, $adminAccount['admin_account_id'], 'admin', $password, $adminAccount['admin_password']);
             break;
         }
     }
@@ -67,13 +67,13 @@ function handle_login_attempts($connect_db, $user_id, $user_type, $password, $st
     global $max_attempts, $lockout_time, $current_time, $error_message;
 
     if ($user_type === 'admin') {
-        $stmtCheckAttempts = mysqli_prepare($connect_db, "SELECT * FROM admin_login_attempts WHERE admin_id = ?");
+        $stmtCheckAttempts = mysqli_prepare($connect_db, "SELECT * FROM admin_attempts WHERE admin_id = ?");
         mysqli_stmt_bind_param($stmtCheckAttempts, 'i', $user_id);
         mysqli_stmt_execute($stmtCheckAttempts);
         $resultCheckAttempts = mysqli_stmt_get_result($stmtCheckAttempts);
         $login_attempt = mysqli_fetch_assoc($resultCheckAttempts);
     } else {
-        $stmtCheckAttempts = mysqli_prepare($connect_db, "SELECT * FROM super_admin_login_attempts WHERE super_admin_id = ?");
+        $stmtCheckAttempts = mysqli_prepare($connect_db, "SELECT * FROM super_admin_attempts WHERE super_admin_id = ?");
         mysqli_stmt_bind_param($stmtCheckAttempts, 'i', $user_id);
         mysqli_stmt_execute($stmtCheckAttempts);
         $resultCheckAttempts = mysqli_stmt_get_result($stmtCheckAttempts);
@@ -107,9 +107,9 @@ function handle_login_attempts($connect_db, $user_id, $user_type, $password, $st
 // Function to reset login attempts
 function reset_attempts($connect_db, $user_id, $user_type) {
     if ($user_type === 'admin') {
-        $stmtReset = mysqli_prepare($connect_db, "DELETE FROM admin_login_attempts WHERE admin_id = ?");
+        $stmtReset = mysqli_prepare($connect_db, "DELETE FROM admin_attempts WHERE admin_id = ?");
     } else {
-        $stmtReset = mysqli_prepare($connect_db, "DELETE FROM super_admin_login_attempts WHERE super_admin_id = ?");
+        $stmtReset = mysqli_prepare($connect_db, "DELETE FROM super_admin_attempts WHERE super_admin_id = ?");
     }
     mysqli_stmt_bind_param($stmtReset, 'i', $user_id);
     mysqli_stmt_execute($stmtReset);
@@ -118,9 +118,9 @@ function reset_attempts($connect_db, $user_id, $user_type) {
 // Function to increment login attempts
 function increment_attempts($connect_db, $user_id, $user_type) {
     if ($user_type === 'admin') {
-        $stmtCheck = mysqli_prepare($connect_db, "SELECT * FROM admin_login_attempts WHERE admin_id = ?");
+        $stmtCheck = mysqli_prepare($connect_db, "SELECT * FROM admin_attempts WHERE admin_id = ?");
     } else {
-        $stmtCheck = mysqli_prepare($connect_db, "SELECT * FROM super_admin_login_attempts WHERE super_admin_id = ?");
+        $stmtCheck = mysqli_prepare($connect_db, "SELECT * FROM super_admin_attempts WHERE super_admin_id = ?");
     }
     mysqli_stmt_bind_param($stmtCheck, 'i', $user_id);
     mysqli_stmt_execute($stmtCheck);
@@ -129,19 +129,19 @@ function increment_attempts($connect_db, $user_id, $user_type) {
 
     if ($attempt) {
         if ($user_type === 'admin') {
-            $stmtUpdate = mysqli_prepare($connect_db, "UPDATE admin_login_attempts SET attempts = attempts + 1, last_attempt_time = NOW() WHERE admin_id = ?");
+            $stmtUpdate = mysqli_prepare($connect_db, "UPDATE admin_attempts SET attempts = admin_number_of_attempts + 1, admin_last_attempt_time = NOW() WHERE admin_id = ?");
         } else {
-            $stmtUpdate = mysqli_prepare($connect_db, "UPDATE super_admin_login_attempts SET attempts = attempts + 1, last_attempt_time = NOW() WHERE super_admin_id = ?");
+            $stmtUpdate = mysqli_prepare($connect_db, "UPDATE super_admin_attempts SET super_admin_number_of_attempts = super_admin_number_of_attempts + 1, super_admin_last_attempt_time = NOW() WHERE super_admin_id = ?");
         }
         mysqli_stmt_bind_param($stmtUpdate, 'i', $user_id);
         mysqli_stmt_execute($stmtUpdate);
     } else {
 
         if ($user_type === 'admin') {
-            $stmtInsert = mysqli_prepare($connect_db, "INSERT INTO admin_login_attempts (admin_id, attempts, first_attempt_time, last_attempt_time) VALUES (?, 1, NOW(), NOW())");
+            $stmtInsert = mysqli_prepare($connect_db, "INSERT INTO admin_attempts (admin_attempts_id,admin_id, admin_number_of_attempts, admin_first_attempt_time, admin_last_attempt_time) VALUES (NULL,?, 1, NOW(), NOW())");
 
         } else {
-            $stmtInsert = mysqli_prepare($connect_db, "INSERT INTO super_admin_login_attempts (super_admin_id, attempts, first_attempt_time, last_attempt_time) VALUES (?, 1, NOW(), NOW())");
+            $stmtInsert = mysqli_prepare($connect_db, "INSERT INTO super_admin_attempts (super_admin_attempts_id,super_admin_id, super_admin_number_of_attempts, super_admin_first_attempt_time, super_admin_last_attempt_time) VALUES (NULL,?, 1, NOW(), NOW())");
         }
         mysqli_stmt_bind_param($stmtInsert, 'i', $user_id);
         mysqli_stmt_execute($stmtInsert);
