ffbe1f5784331522f5e9341ca793e1a882f02b96
Izumi <162671709+Yoro-Izumi@users.noreply.github.com>
2024-09-29 09:13:27 +0800

    changes in log in code

--- Admin/Login-page/js/submit_form.js
+++ Admin/Login-page/js/submit_form.js
@@ -60,3 +60,33 @@ $(document).ready(function () {
 }); 
 
     
+/*
+$(document).ready(function() {
+    $('#myForm').submit(function(event) {
+        event.preventDefault(); // Prevent the form from submitting via the browser
+
+        var formData = $(this).serialize();
+
+        $.ajax({
+            type: 'POST',
+            url: 'process.php',
+            data: formData,
+            success: function(response) {
+                if (response === "Form submitted successfully!") {
+                    $('#modalMessage').text(response);
+                } else if (response === "Failed to submit the form. Please try again.") {
+                    $('#modalMessage').text(response);
+                } else {
+                    $('#modalMessage').text("Unexpected response: " + response);
+                }
+                $('#responseModal').modal('show');
+            },
+            error: function(xhr, status, error) {
+                $('#modalMessage').text('An error occurred: ' + xhr.responseText);
+                $('#responseModal').modal('show');
+            }
+        });
+    });
+});
+
+*/
